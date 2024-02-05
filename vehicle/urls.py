0d7f46291ae2922d55d14a48be93444498895a2a
olga voronets <voronets80@gmail.com>
2024-02-05 13:34:58 +0300

    Задача Создать модель «Список мотоциклов» и реализовать механизм CRUD на основе Generic.
    Шаги решения
    Создать модель.
    Описать сериализатор.
    Описать контроллеры на основе Generics для каждого действия механизма CRUD.
    Описать урлы.

--- vehicle/urls.py
+++ vehicle/urls.py
@@ -1,12 +1,19 @@
+from django.urls import path
+
 from vehicle.apps import VehicleConfig
 from rest_framework.routers import DefaultRouter
 
-from vehicle.views import CarViewSet
+from vehicle.views import CarViewSet, MotoCreateView, MotoListView, MotoRetrieveView, MotoUpdateView, MotoDestroyView
 
 app_name = VehicleConfig.name
 router = DefaultRouter()
 router.register(r'cars', CarViewSet, basename='cars')
 
 urlpatterns = [
+                  path('moto/create/', MotoCreateView.as_view(), name='moto_create'),
+                  path('moto/', MotoListView.as_view(), name='moto_list'),
+                  path('moto/<int:pk>/', MotoRetrieveView.as_view(), name='moto_view'),
+                  path('moto/update/<int:pk>/', MotoUpdateView.as_view(), name='moto_update'),
+                  path('moto/delete/<int:pk>/', MotoDestroyView.as_view(), name='moto_delete'),
 
-] + router.urls
+              ] + router.urls
